29f95af895d5dc7b6ea2b7f9a08b640f058ed636
mssj-11 <michael11.salgado10@gmail.com>
2024-12-11 21:54:17 -0600

    Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
   const [navHeight, setNavHeight] = useState(false);
   const navigate = useNavigate();
-  const gotoHome = ()=>{
+
+  const gotoHome = () => {
     navigate('/');
-  }
+  };
+
   return (
     <>
       <nav className={navHeight ? "show nav" : "nav"}>
-        <div className="logo" onClick={()=> gotoHome()}>LUXURY RENTALS</div>
+        <div className="logo" onClick={() => gotoHome()}>
+          LUXURY RENTALS
+        </div>
         <ul>
           <li>
             <Link to={"/about"}>ABOUT US</Link>
@@ -23,10 +27,18 @@ const Navbar = () => {
             <Link to={"/contact"}>CONTACT</Link>
           </li>
         </ul>
-        <RxHamburgerMenu
-          className="hamburger"
-          onClick={() => setNavHeight(!navHeight)}
-        />
+        {/* Alternar entre RxHamburgerMenu y RxCross2 */}
+        {navHeight ? (
+          <RxCross2
+            className="hamburger"
+            onClick={() => setNavHeight(false)}
+          />
+        ) : (
+          <RxHamburgerMenu
+            className="hamburger"
+            onClick={() => setNavHeight(true)}
+          />
+        )}
       </nav>
     </>
   );
